f7c77199b36dc728f64ce6203dd633fdbfda946c
wsxq2 <wsxq2@qq.com>
2019-10-10 14:20:08 +0800

    make PROXY in config.sh more robust, add PIP_INDEX_URL in config.sh, add timer for w4sp_init.sh

--- config.sh
+++ config.sh
@@ -3,9 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
+PIP_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
 PROXY=socks5h://127.0.0.1:1081
 #PROXY=socks5h://192.168.56.100:1082
 #PROXY=http://192.168.56.100:1081
-grep -rlI --exclude config.sh 'http://192.168.56.100:1081' . | xargs sed -i -e "/http:\/\/192.168.56.100:1081/s##$PROXY#g"
+
+find . -type f -name Dockerfile |xargs grep -l PIP_INDEX_URL | xargs sed -i -e "/PIP_INDEX_URL/s##$PIP_INDEX_URL#g"
+find . -type f -name Dockerfile |xargs grep -l PROXY | xargs sed -i -e "/PROXY/s##$PROXY#g"
 
 #cd images/base/ && [[ -f source.list.xidian ]] && mv -f source.list.xidian source.list && cd -
--- w4sp_init.sh
+++ w4sp_init.sh
@@ -2,7 +2,21 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [ $USER != 'root' ]
+then
+	echo "Please login with root or run with sudo!"
+	exit -1
+fi
 
+login_name=$(logname)
+if [ $login_name = 'root' || $login_name != 'w4sp-lab' ]
+then
+	id w4sp-lab || (echo 'so create user w4sp-lab' && useradd -mU -s /bin/bash -G sudo,wireshark -p '$6$teB23uhk$mUIdkn2Gby0viZxfexKSXTnc6leh1qGdtljSVSQkAKk3kzpTmcVYk/.h6TWJxFm2GtQZ1Wa3rHDlIZgrjT9nX.' w4sp-lab)
+	echo "Please login with w4sp-lab and run this script again(sudo bash $0)"
+	exit 0
+fi
+
+declare -i start_time=$(date +%s)
 wget -O /etc/apt/sources.list https://raw.githubusercontent.com/wsxq2/MyProfile/master/Linux/Kali/etc/apt/sources.list
 
 #echo '# for X11
@@ -29,11 +43,13 @@ echo '{
 }
 ' > /etc/docker/daemon.json
 
-id w4sp-lab || (echo 'so create user w4sp-lab' && useradd -mU -s /bin/bash -G sudo,wireshark -p '$6$teB23uhk$mUIdkn2Gby0viZxfexKSXTnc6leh1qGdtljSVSQkAKk3kzpTmcVYk/.h6TWJxFm2GtQZ1Wa3rHDlIZgrjT9nX.' w4sp-lab)
-
 sed -i '/apt.dockerproject.org/s/^#//'  /etc/apt/sources.list
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D
 
-su - w4sp-lab -c 'cd ~ && [[ ! -d w4sp-lab/ ]] && git clone https://github.com/wsxq2/w4sp-lab.git'
+bash config.sh
 
+declare -i end_time=$(date +%s)
+echo "cost $(($end_time-$start_time)) s"
+
+echo 'please run `sudo python w4sp_webapp.py`'
